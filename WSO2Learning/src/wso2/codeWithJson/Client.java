a5b5fcdccad1b86bf3c9525056d7c67b52a0197c
irunika <irunikail@gmail.com>
2015-11-10 14:59:16 +0530

    Made a simple client and a server

--- WSO2Learning/src/wso2/codeWithJson/Client.java
+++ WSO2Learning/src/wso2/codeWithJson/Client.java
@@ -1,21 +1,34 @@
 package wso2.codeWithJson;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.PrivilegedExceptionAction;
+import java.util.Scanner;
 
 /**
  * Created by irunika on 11/10/15.
  */
-public class Client implements  Runnable {
+public class Client{
+    public static void main(String[] args){
 
-    private Socket socket;
+        try {
+            InetAddress address = InetAddress.getByName("localhost");
 
-    public Client(Socket s){
-        this.socket = s;
+            Socket s = new Socket(address, Server.port);
+            Scanner scan = new Scanner(s.getInputStream());
+
+            PrintStream out = new PrintStream(s.getOutputStream());
+            out.println("Hi Server");
+            System.out.println(scan.nextLine());
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void run() {
-
-    }
 }
--- WSO2Learning/src/wso2/codeWithJson/Server.java
+++ WSO2Learning/src/wso2/codeWithJson/Server.java
@@ -3,13 +3,19 @@ package wso2.codeWithJson;
 import sun.tools.jar.CommandLine;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by irunika on 11/10/15.
  */
 public class Server {
+
+    public final static int port =  22000;
+
     public static void main(String[] args){
         final int port = 22000;
 
@@ -20,6 +26,12 @@ public class Server {
             while (true){
                 Socket s = serverSocket.accept();  // Accept the cliet who is trying to connect
                 System.out.println("Client is connected from " + s.getLocalAddress().getHostName() );
+
+                Scanner scan = new Scanner(s.getInputStream());
+                System.out.println(scan.nextLine());
+
+                PrintStream out = new PrintStream(s.getOutputStream());
+                out.println("Hi Client");
 
             }
         } catch (IOException e) {
